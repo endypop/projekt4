6893ac4fd64da4159d45c184ac95f3fe98529736
Patryk Czarnik <patryk@patrykczarnik.net>
2022-03-04 16:10:17 +0100

    Ala Ola

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello ");
-        System.out.println("Hello ");
-        System.out.println("Hello ");
-        System.out.println("Hello ");
-        System.out.println("Hello ");
+        System.out.println("Hello Ala");
+        System.out.println("Hello Ola");
+        System.out.println("Hello Ela");
+        System.out.println("Hello Ula");
+        System.out.println("Hello Aga");
     }
 }
