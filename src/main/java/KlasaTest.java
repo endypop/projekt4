0e4aa0d85a497ae05962529109b8e8474f3ab519
Andrzej <2452@sdf>
2022-03-04 16:10:17 +0100

    Założenie projektu IntelliJ v6

---
+++ src/main/java/KlasaTest.java
@@ -0,0 +1,8 @@
+public class KlasaTest {
+    public static void main(String[] args) {
+        System.out.println("Nowa klasa1");
+        System.out.println("Nowa klasa2");
+        System.out.println("Nowa klasa3");
+        System.out.println("Nowa klasa4");
+    }
+}
