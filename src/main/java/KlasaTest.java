fbefb869c3fc9efc1a6ce6284e9db4417863585b
Andrzej <2452@sdf>
2022-03-04 16:10:41 +0100

    Założenie projektu IntelliJ v7

--- src/main/java/KlasaTest.java
+++ src/main/java/KlasaTest.java
@@ -4,5 +4,8 @@ public class KlasaTest {
         System.out.println("Nowa klasa2");
         System.out.println("Nowa klasa3");
         System.out.println("Nowa klasa4");
+        System.out.println("Nowa klasa5");
+        System.out.println("Nowa klasa6");
+        System.out.println("Nowa klasa7");
     }
 }
