ef9b8639eaae39e40499b8e48b9186457e54d01c
Andrzej <2452@sdf>
2022-03-04 15:50:53 +0100

    Założenie projektu IntelliJ v4

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -8,5 +8,13 @@ public class Test {
         System.out.println("3333333333333333333");
         System.out.println("1111111111111111111");
         System.out.println("1111111111111111111");
+        System.out.println("1111111111111111111");
+        System.out.println("4444444444444444444");
+        System.out.println("4444444444444444444");
+        System.out.println("4444444444444444444");
+        System.out.println("4444444444444444444");
+        System.out.println("4444444444444444444");
+        System.out.println("4444444444444444444");
+        System.out.println("4444444444444444444");
     }
 }
